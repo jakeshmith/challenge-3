5a55e5457051f4c798e23b23c79de14d7df23730
jakeshmith <jakerusmith@gmail.com>
2023-12-22 19:06:31 -0600

    finished project, i hope

--- Develop/script.js
+++ Develop/script.js
@@ -4,151 +4,195 @@ var charNumb = "0123456789";
 var charLower = "abcdefghijklmnopqrstuvwxyz"
 var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var specChar = "!@#$%^&*()_-+={[}]:;'<,>.?/~`"
+var findLength;
+var findUpperCase;
+var findLowerCase;
+var findNumbers;
+var findSpecial;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-function findLength();
-  if (passwordLength<8) {
+function determineLength(){
+  findLength = prompt("How long you want that thang to be?")
+  if (findLength<8) {
     alert("Password length must be between 8 and 128 characters long");
     findLength();
-  } else if (passwordLength>128) {
+  } else if (findLength>128) {
     alert("Password length must be between 8 and 128 characters long");
     findLength;
   } else {
     alert("Way to go! What characters would you like to use?")
   }
+  return findLength;
+}
 
-  function  findUpperCase(){
-    uppercaseCheck = prompt("Do you want uppercase letters in your password? \(Yes or No)");
-      uppercaseCheck = uppercaseCheck.toLowerCase();
+  function  determineUppercase(){
+    findUpperCase = prompt("Do you want uppercase letters in your password? \(Yes or No)");
+      findUpperCase = findUpperCase.toLowerCase();
 
-      if (uppercaseCheck === null || uppercaseCheck === ""){
+      if (findUpperCase === null || findUpperCase === ""){
         alert("Yes or No");
         determineUppercase();
 
-      }else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
-        uppercaseCheck = true;
-        return uppercaseCheck;
+      }else if (findUpperCase === "yes" || findUpperCase ==="y"){
+        findUpperCase = true;
+        return findUpperCase;
+
+      }else if (findUpperCase === "no" || findUpperCase ==="n"){
+        findUpperCase = false;
+      return findUpperCase;
 
       }else {
-        alert("Please answer Yes or No");
+        alert("Yes or No");
         determineUppercase();
+      }
+      return findUpperCase;
+    }
 
-      }else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
-        uppercaseCheck = true;
-        return uppercaseCheck;
-
-      }else if (uppercaseCheck === "no" || uppercaseCheck ==="n"){
-        uppercaseCheck = false;
-      return uppercaseCheck;
-
-  }else {
-    alert("Yes or No");
-    determineUppercase();
-  }
-  return uppercaseCheck;
-  }
+    function  determineLowercase(){
+      findLowerCase = prompt("Do you want lowercase letters in your password? \(Yes or No)");
+        findLowerCase = findLowerCase.toLowerCase();
+ 
+  
+        if (findLowerCase === "yes" || findLowerCase ==="y"){
+          findLowerCase = true;
+          return findLowerCase;
+  
+        }else if (findLowerCase === "no" || findLowerCase ==="n"){
+          findLowerCase = false;
+        return findLowerCase;
+  
+        }else {
+          alert("Yes or No");
+          determineLowercase();
+        }
+        return findLowerCase;
+    }
 
 
   function determineNumbers(){
-    numberCheck = prompt("Would you like numbers in your password? \(Yes or No)");
-      numberCheck = numberCheck.toLowerCase();
+    findNumbers = prompt("Would you like numbers in your password? \(Yes or No)");
+      findNumbers = findNumbers.toLowerCase();
 
-      if (numberCheck === null || numberCheck === ""){
-        alert("Please answer Yes or No");
+      if (findNumbers === null || findNumbers === ""){
+        alert("Yes or No");
         determineNumbers();
 
-  }else if (numberCheck === "yes" || numberCheck ==="y"){
-    numberCheck = false;
-    return numberCheck;
+      }else if (findNumbers === "yes" || findNumbers ==="y"){
+        findNumbers = true;
+        return findNumbers;
 
-  }else {
-    alert("Yes or No");
-    determineNumbers();
-  }
-  return numberCheck;
+      }else if (findNumbers === "no" || findNumbers ==="n"){
+        findNumbers = false;
+        return findNumbers;
+
+    }else {
+      alert("Yes or No");
+      determineNumbers();
+    }
+    return findNumbers;
 }
 
   function determineSpecial(){
-    specialCheck = prompt("Do you want to include special characters in your password? \n(Yes or No)");
-      specialCheck = specialCheck.toLowerCase();
+    findSpecial = prompt("Want me throw some special characters in there for ya? \n(Yes or No)");
+      findSpecial = findSpecial.toLowerCase();
 
-      if (specialCheck === null || specialCheck === ""){
+      if (findSpecial === null || findSpecial === ""){
         alert("Please answer Yes or No");
         determineSpecial();
 
-      }else if (specialCheck === "yes" || specialCheck ==="y"){
-        specialCheck = true;
-        return specialCheck;
+      }else if (findSpecial === "yes" || findSpecial ==="y"){
+        findSpecial = true;
+        return findSpecial;
 
-      }else if (specialCheck === "no" || specialCheck ==="n"){
-        specialCheck = false;
-        return specialCheck;
+      }else if (findSpecial === "no" || findSpecial ==="n"){
+        findSpecial = false;
+        return findSpecial;
       
       }else {
         alert("Please answer Yes or No");
         determineSpecial();
       }
-      return specialCheck;
+      return findSpecial;
   }
 
   function generatePassword(){
-    determineLength();
-  console.log(passwordLength);
+  determineLength();
+  console.log(findLength);
   determineUppercase();
-  console.log(uppercaseCheck);
+  console.log(findUpperCase);
+  determineLowercase();
+  console.log(findUpperCase);
   determineNumbers();
-  console.log(numberCheck);
+  console.log(findNumbers);
   determineSpecial();
-  console.log(specialCheck);
+  console.log(findSpecial);
 
-  var characters = lowercaseChar;
+  var characters = charLower;
   var password = "";
-  if (uppercaseCheck && numberCheck && specialCheck){
-    characters += uppercaseChar + numberChar + specialChar;
 
-  }else if (uppercaseCheck && numberCheck){
-  characters += uppercaseChar + numberChar;
+  if (findUpperCase && findLowerCase && findNumbers && findSpecial){
+    characters += charUpper + charLower + charNumb + specChar;
 
-}else if (numberCheck && specialCheck){
-  characters += numberChar + specialChar;
+  }else if (findUpperCase && findLowerCase && findNumbers){
+    characters += charUpper + charLower && charNumb;
+    
+  }else if (findUpperCase && findLowerCase && findSpecial){
+    characters += charUpper + charLower && specChar;
+    
+  }else if (findUpperCase && findSpecial && findNumbers){
+    characters += charUpper + specChar && charNumb;
+    
+  }else if (findLowerCase && findSpecial && findNumbers){
+    characters += charLower + specChar && charNumb;
 
-}else if (uppercaseCheck && specialCheck){
-  characters += uppercaseChar + specialChar;
+  }else if (findUpperCase && findNumbers){
+  characters += charUpper + charNumb;
 
-}else if (uppercaseCheck){
-  characters += uppercaseChar;
+}else if (findNumbers && findSpecial){
+  characters += charNumb + specChar;
 
-}else if(numberCheck){
-  characters += numberChar;
+}else if (findUpperCase && findSpecial){
+  characters += charUpper + specChar;
 
-}else if (specialCheck){
-  characters += specialChar;
+}else if (findLowerCase && findNumbers){
+  characters += charLower + charNumb;
+
+}else if (findLowerCase && findSpecial){
+  characters += charLower + specChar;
+
+}else if (findLowerCase && findUpperCase){
+  characters += charLower + charUpper;
+
+}else if (findUpperCase){
+  characters += charUpper;
+
+}else if(findNumbers){
+  characters += charNumb;
+
+}else if (findSpecial){
+  characters += specChar;
+
+}else if (findLowerCase){
+  characters += charLower;
 
 }else{
   characters === lowercaseChar;
 }
 
-for(var i = 0; i < passwordLength; i++){
+for (var i = 0; i < findLength; i++){
   password += characters.charAt(Math.floor(Math.random() * characters.length));
 }
 return password;
 }
 
-// Write password to the #password input
+
 function writePassword() {
-var password1 = "";
-password1 = generatePassword();
-var passwordText = document.querySelector("#password");
-passwordText.value = password1;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 }
 
 
-  passwordText.value = password;
 
+document.getElementById("password").value = "Your Secure Password";
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
